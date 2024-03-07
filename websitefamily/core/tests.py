4b3467938eb33dc06dd5a66e302619a51eeac2f6
mohamd <mohamdsleman68@gmail.com>
2024-03-07 21:43:28 +0200

    second commit

--- websitefamily/core/tests.py
+++ websitefamily/core/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test.client import RequestFactory
+from django.urls import reverse
+from myapp.views import YourFormView
+from myapp.forms import YourForm
 
-# Create your tests here.
+
+class YourFormViewTests(TestCase):
+    def test_form_valid(self):
+        request = RequestFactory().post(reverse('your_form_view_url'),
+                                        data={})
+        user = User.objects.create(username='testuser')
+        form = YourForm(data={}, instance=user)
+        form.is_valid = lambda: True
+        view = YourFormView()
+        view.request = request
+        response = view.form_valid(form)
+
+        self.assertRedirects(response,
+                             reverse('profile_url'))
--- websitefamily/core/urls.py
+++ websitefamily/core/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path , include
 from .views import *
+from . import views
+from .views import login_page
+
+
 urlpatterns = [
     path('',login_page,name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', logout_user,name='logout'),
     path('profile/', profile,name='profile'),
+
 
 ]
