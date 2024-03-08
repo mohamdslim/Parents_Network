4b4a61b81a559f5c16e98767c7ec34c864fa4420
mohamd <mohamdsleman68@gmail.com>
2024-03-08 12:11:45 +0200

    first commit

--- websitefamily/core/tests.py
+++ websitefamily/core/tests.py
@@ -1,21 +1,88 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-from django.test.client import RequestFactory
+from django.contrib.auth.models import User , logout
 from django.urls import reverse
-from myapp.views import YourFormView
-from myapp.forms import YourForm
 
 
-class YourFormViewTests(TestCase):
-    def test_form_valid(self):
-        request = RequestFactory().post(reverse('your_form_view_url'),
-                                        data={})
-        user = User.objects.create(username='testuser')
-        form = YourForm(data={}, instance=user)
-        form.is_valid = lambda: True
-        view = YourFormView()
-        view.request = request
-        response = view.form_valid(form)
+def test_logout_user(request):
+    # ודא שהמשתמש מחובר
+    user = request.user
+    self.assertTrue(user.is_authenticated)
 
-        self.assertRedirects(response,
-                             reverse('profile_url'))
+    response = logout_user(request)
+
+    self.assertFalse(user.is_authenticated)
+
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(response.url, reverse('login'))
+
+#  3 בדיקות יחידה מ כלי AI
+# Cloude.AI
+#1 unittest 1
+class SignupViewTest(TestCase):
+
+    def test_signup_user(self):
+        # הכנת נתוני משתמש
+        data = {
+            'username': 'mohamad',
+            'email': 'mohamad.2@gmail.com',
+            'password1': 'Moh12345',
+            'password2': 'Moh12345',
+        }
+
+        response = self.client.post(reverse('signup'), data=data)
+
+        # בדיקת יצירת משתמש חדש
+        self.assertEqual(User.objects.count(), 1)
+        user = User.objects.get(username='mohamad')
+
+        # בדיקת התחברות אוטומטית
+        self.assertTrue(self.client.login(username='mohamad', password='Moh12345'))
+
+        # בדיקת הפניית משתמש לדף הפרופיל
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('profile'))
+
+#Unittest 2
+
+
+class SignupViewTest(TestCase):
+
+    def test_signup_user_with_mismatched_passwords(self):
+        # הכנת נתוני משתמש
+        data = {
+            'username': 'mohamad',
+            'email': 'mohamad.2@gmail.com',
+            'password1': 'Moh12345',
+            'password2': 'Moh12345',
+        }
+
+        response = self.client.post(reverse('signup'), data=data)
+
+        # בדיקת אי יצירת משתמש חדש
+        self.assertEqual(User.objects.count(), 1)
+
+        # בדיקת קבלת שגיאה
+        self.assertFormError(response, 'form', 'username', 'This username is already taken.')
+
+
+
+
+# Unittest 3
+
+
+
+class SignupViewTest(TestCase):
+
+    def test_signup_user_with_existing_username(self):
+        User.objects.create_user(username='test_user', password='password1234')
+
+        # הכנת נתוני משתמש
+        data = {
+            'username': 'mohamad',
+            'email': 'mohamad.2@gmail.com',
+            'password1': 'Moh12345',
+            'password2': 'Moh12345',
+        }
+
+        response = self.client.post(reverse('signup'), data=data)
+
