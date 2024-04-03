3381863295fb5f25cc08ed99f1092b569b599905
mohamdslim <121812423+mohamdslim@users.noreply.github.com>
2024-04-03 09:33:28 +0300

    forms.py

--- projectnetworkparents/core/forms.py
+++ projectnetworkparents/core/forms.py
@@ -1,1 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from django import forms
+from django.forms import ModelForm
+
+class SignupForm(UserCreationForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
 
